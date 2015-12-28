a4be7113d849774e35b029daef9422eef308d349
Mangos Lassi <mangoslassi@gmail.com>
2015-12-28 03:16:15 -0500

    Initial code and data commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+FeenixCensus_Backup
+====
+This project is intended to modestly backup as many ED players as possible from FeenixCensus.
+
---
+++ feenixcensus_backup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# feenixcensus_backup.py
+# Create backups of FeenixCensus day by day.
+# This is intended to preserve as many
+#   ED character names as possible.
+
+import requests
+from calendar import monthrange
+
+baseurl = 'http://www.feenixcensus.com/?'
+
+year = 2014
+for month in range(12, 13):
+    days = monthrange(year, month)[1]
+    while days > 1:
+        if days - 7 >= 1:
+            begin_day = days - 7
+            end_day = days
+            days -= 7
+        else:
+            begin_day = 1
+            end_day = days
+            days = 1
+        searchQuery = 'start_level=60&start_date=' + str(year) + '-' + str(month) + '-' + str(begin_day) + '&end_date=' + str(year) + '-' + str(month) + '-' + str(end_day) + '&search=Search'
+        page = requests.get(baseurl + searchQuery)
+        with open(searchQuery, 'w') as f:
+            f.write(page.text)
+
